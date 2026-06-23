ffd4ad06a32fee2db610d420f3f05c0ab2fc4383
kallcyte <hello@kall.dev>
2026-06-23 09:51:23 +0700

    add cloudflare integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,13 +4,18 @@ import { defineConfig } from "astro/config";
 import alpinejs from "@astrojs/alpinejs";
 import tailwindcss from "@tailwindcss/vite";
 
+import cloudflare from "@astrojs/cloudflare";
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+
   image: {
     layout: "full-width",
   },
+
   integrations: [alpinejs({ entrypoint: "/src/utils/script.ts" })],
+  adapter: cloudflare(),
 });
